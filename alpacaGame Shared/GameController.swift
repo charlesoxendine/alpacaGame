c68cec507dafbfa6f7ddf2563d50c3e032d63512
coxendin <coxendin@starbucks.com>
2023-05-12 19:19:19 -0400

    updated code hehe

--- alpacaGame Shared/GameController.swift
+++ alpacaGame Shared/GameController.swift
@@ -26,8 +26,11 @@ class GameController: NSObject, SCNSceneRendererDelegate {
         
         sceneRenderer.delegate = self
         sceneRenderer.scene = scene
+        
+        renderFieldSheep()
     }
     
+    // TODO: Feedback for tapping the factory
     func highlightNodes(atPoint point: CGPoint) {
         let hitResults = self.sceneRenderer.hitTest(point, options: [:])
         for result in hitResults {
@@ -40,6 +43,31 @@ class GameController: NSObject, SCNSceneRendererDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         // Called before each frame is rendered
     }
-
     
+    func renderFieldSheep() {
+        for num in 1...10 {
+            let node = SCNNode(named: "Art.scnassets/llama.dae")
+            let randomX:Float = Float.random(in: -1.5 ..< 1.5)
+            let randomY:Float = 0
+            let randomZ:Float = Float.random(in: -1 ..< 1)
+            
+            node.position = SCNVector3(x: -0.436 + randomX, y: 0.042 + randomY, z: -1.766 + randomZ)
+            scene.rootNode.addChildNode(node)
+        }
+    }
+}
+
+// TODO: MOVE THIS
+extension SCNNode {
+    convenience init(named name: String) {
+        self.init()
+
+        guard let scene = SCNScene(named: name) else {
+            return
+        }
+
+        for childNode in scene.rootNode.childNodes {
+            addChildNode(childNode)
+        }
+    }
 }
